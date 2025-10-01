5832d06b6026d650619d55de5c2c707bb0684ea4
shubham <shubhamjatti01@gmail.com>
2025-10-01 22:09:27 +0530

    index

---
+++ sun.js
@@ -0,0 +1,28 @@
+<script>
+  const toggleDiv = document.getElementById("theme-toggle");
+  const sunIcon = document.getElementById("theme-toggle-sun");
+  const moonIcon = document.getElementById("theme-toggle-moon");
+
+  // Check saved theme or system preference
+  if (localStorage.theme === 'dark' || 
+     (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    sunIcon.classList.remove("hidden");
+  } else {
+    moonIcon.classList.remove("hidden");
+  }
+
+  toggleDiv.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.theme = "dark";
+      sunIcon.classList.remove("hidden");
+      moonIcon.classList.add("hidden");
+    } else {
+      localStorage.theme = "light";
+      sunIcon.classList.add("hidden");
+      moonIcon.classList.remove("hidden");
+    }
+  });
+</script>
